60ac0a4b97dc007600355016d5a2d7665424a420
Mateusz Czapliński <czapkofan@gmail.com>
2014-03-16 17:13:56 +0100

    flatten typedefs over typedefs to underlying primitive types

--- h2go/c3printer.go
+++ h2go/c3printer.go
@@ -8,9 +8,14 @@ import (
 type Printer struct {
 	W          *bufio.Writer
 	CurlyDepth int
+	Flatten    map[string]string
 }
 
 func (p *Printer) emit(d Decl, decor DecoratedType, ident, ornaments, typenameGo, original string) error {
+	if p.Flatten == nil {
+		p.Flatten = make(map[string]string)
+	}
+
 	if p.CurlyDepth == 0 && d.Typedef {
 		p.W.WriteString("type ")
 	} else if p.CurlyDepth == 0 {
@@ -23,19 +28,21 @@ func (p *Printer) emit(d Decl, decor DecoratedType, ident, ornaments, typenameGo
 
 	writeindent(p.W, p.CurlyDepth)
 
-	if ident != "" {
-		p.W.WriteString(upcase(ident) + " ")
-	} else {
+	if ident == "" {
 		return fmt.Errorf("anonymous structs/enums not supported")
 	}
+	p.W.WriteString(upcase(ident) + " ")
 
 	p.W.WriteString(ornaments)
 
-	if typenameGo != "" {
-		p.W.WriteString(typenameGo)
-	} else {
+	if typenameGo == "" {
 		return fmt.Errorf("untyped declarations not supported")
 	}
+	if v := p.Flatten[typenameGo]; v != "" {
+		typenameGo = v
+	}
+	p.W.WriteString(typenameGo)
+	p.Flatten[ident] = typenameGo
 
 	p.W.WriteString("\t// " + original)
 	p.W.WriteString("\n")
